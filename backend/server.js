eba811ab7e2c5b64be74a56587bcb7556357d33e
Shanur <shanurislam@gmail.com>
2024-03-03 02:12:11 +0000

    Completed message controllers
    - Created two new endpoints for sending and receiving messages
    - Created models for conversations and messages
    - Created a function to protectRoute to ensure messages can only be sent and read by logged in users

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -10,8 +12,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // to parse the incoming requrest with JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
